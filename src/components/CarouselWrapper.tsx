5f9fb8a3a2bd14f6dc0b1ee3d1c1c88446be40b0
jpcpais01 <jpcpais01@gmail.com>
2024-12-14 00:47:11 +0000

    tiny fixes and additions

--- src/components/CarouselWrapper.tsx
+++ src/components/CarouselWrapper.tsx
@@ -6,6 +6,7 @@ import Journal from './pages/Journal'
 import Chat from './pages/Chat'
 import Resources from './pages/Resources'
 import Navigation from './Navigation'
+import SplashScreen from './SplashScreen'
 
 export default function CarouselWrapper() {
   const [emblaRef, emblaApi] = useEmblaCarousel({ 
@@ -47,27 +48,30 @@ export default function CarouselWrapper() {
   }
 
   return (
-    <div className="h-screen flex flex-col bg-gradient-to-b from-[#f8fafc] to-[#f1f5f9] dark:from-neutral-900 dark:to-neutral-950">
-      <div className="flex-1 overflow-hidden transform-gpu" ref={emblaRef}>
-        <div className="flex h-full will-change-transform">
-          <div className="flex-[0_0_100%] min-w-0 relative p-4 transform-gpu">
-            <div className="h-full max-w-2xl mx-auto">
-              {selectedIndex === 0 || selectedIndex === 1 ? <Journal /> : null}
+    <>
+      <SplashScreen />
+      <div className="h-screen flex flex-col bg-gradient-to-b from-[#f8fafc] to-[#f1f5f9] dark:from-neutral-900 dark:to-neutral-950">
+        <div className="flex-1 overflow-hidden transform-gpu" ref={emblaRef}>
+          <div className="flex h-full will-change-transform">
+            <div className="flex-[0_0_100%] min-w-0 relative p-4 transform-gpu">
+              <div className="h-full max-w-2xl mx-auto">
+                {selectedIndex === 0 || selectedIndex === 1 ? <Journal /> : null}
+              </div>
             </div>
-          </div>
-          <div className="flex-[0_0_100%] min-w-0 relative p-4 transform-gpu">
-            <div className="h-full max-w-2xl mx-auto">
-              <Chat />
+            <div className="flex-[0_0_100%] min-w-0 relative p-4 transform-gpu">
+              <div className="h-full max-w-2xl mx-auto">
+                <Chat />
+              </div>
             </div>
-          </div>
-          <div className="flex-[0_0_100%] min-w-0 relative p-4 transform-gpu">
-            <div className="h-full max-w-2xl mx-auto">
-              {selectedIndex === 1 || selectedIndex === 2 ? <Resources /> : null}
+            <div className="flex-[0_0_100%] min-w-0 relative p-4 transform-gpu">
+              <div className="h-full max-w-2xl mx-auto">
+                {selectedIndex === 1 || selectedIndex === 2 ? <Resources /> : null}
+              </div>
             </div>
           </div>
         </div>
+        <Navigation onNavigate={scrollTo} activeIndex={selectedIndex} />
       </div>
-      <Navigation onNavigate={scrollTo} activeIndex={selectedIndex} />
-    </div>
+    </>
   )
 }
